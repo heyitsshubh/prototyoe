c67bdeb86a70ed7be75d426d79e52c88fa2a31e2
unknown <shubhgupta4102@gmail.com>
2026-02-11 05:11:19 +0530

    Fix model path to use relative URL instead of hardcoded domain

--- client/src/config/products.js
+++ client/src/config/products.js
@@ -2,7 +2,7 @@ export const PRODUCTS = {
   jacket1: {
     id: "jacket1",
     name: "Winter Jacket",
-    model: "https://prototyoe.vercel.app/models/jacket.glb",
+    model: "/models/jacket.glb",
     defaultColor: "#222222"
   }
 };
